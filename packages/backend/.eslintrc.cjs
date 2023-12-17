6d336656e350bf97dd8a841cf5c91a743c27e4cf
kitou88C39 <kanfutrooper@icloud.com>
2023-12-17 11:20:51 +0900

    Edit/backend/src/.eslintrc.cjs

---
+++ packages/backend/.eslintrc.cjs
@@ -0,0 +1,31 @@
+module.exports = {
+  env: {
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
+  ],
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: ['.eslintrc.{js,cjs}'],
+      parserOptions: {
+        sourceType: 'script',
+        tsconfigRootDir: __dirname,
+        project: ['tsconfig.eslint.json'],
+      },
+    },
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint'],
+  rules: {},
+};
