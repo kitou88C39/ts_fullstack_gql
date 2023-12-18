702a02082cb08808473cd6d1d7274896108e08a1
kitou88C39 <kanfutrooper@icloud.com>
2023-12-18 13:48:36 +0900

    Refactor backend/src/index.ts

--- packages/backend/src/index.ts
+++ packages/backend/src/index.ts
@@ -5,6 +5,7 @@ import { ApolloServerPluginDrainHttpServer } from '@apollo/server/plugin/drainHt
 import { expressMiddleware } from '@apollo/server/express4';
 import express from 'express';
 import { mergeResolvers, mergeTypeDefs } from '@graphql-tools/merge';
+import { MyContext } from './types/graphql.js';
 
 async function main() {
   const PORT = process.env.PORT || 5555;
