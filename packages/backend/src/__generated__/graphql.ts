2989ca30761820df3ad01f44905a50f9dbcc90b4
kitou88C39 <kanfutrooper@icloud.com>
2023-12-19 10:21:59 +0900

    Edit/backend/src/__generated__/graphql.ts

--- packages/backend/src/__generated__/graphql.ts
+++ packages/backend/src/__generated__/graphql.ts
@@ -1,6 +1,12 @@
 import { GraphQLResolveInfo } from 'graphql';
 export type Maybe<T> = T | null;
 export type InputMaybe<T> = Maybe<T>;
+export type Exact<T extends { [key: string]: unknown }> = {
+  [K in keyof T]: T[K];
+};
+export type MakeOptional<T, K extends keyof T> = Omit<T, K> & {
+  [SubKey in K]?: Maybe<T[SubKey]>;
+};
 
 export type scalars = {
   ID: number;
