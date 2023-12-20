c8d6c6fb35748485e02a68b92a98cd63ede7e363
kitou88C39 <kanfutrooper@icloud.com>
2023-12-20 10:30:23 +0900

    Add buildSchema.ts in the utils folder

--- packages/backend/src/utils/buildSchema.ts
+++ packages/backend/src/utils/buildSchema.ts
@@ -1,11 +1,17 @@
 import fs from 'fs';
 import path from 'path';
-import glob from 'glob';
+import { glob } from 'glob';
 import { mergeResolvers, mergeTypeDefs } from '@graphql-tools/merge';
 import { makeExecutableSchema } from '@graphql-tools/schema';
 
-export const buildSchema = () => {
+export const buildSchema = async () => {
   const __dirname = path.dirname(new URL(import.meta.url).pathname);
   const pathToModules = path.join(__dirname, '..', 'modules');
   const pathToResolvers = path.join(pathToModules, '**', '*.resolvers.?s');
+  const resolversPromises = glob
+    .sync(pathToResolvers)
+    .map((resloversFile) => import(resloversFile));
+  const resolvers = (await Promise.all(resolversPromises)).map(
+    (fileItems) => fileItems.resolvers
+  );
 };
