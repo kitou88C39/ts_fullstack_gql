188e2622cb77d53a42af5ae4167da3343ad4e771
kitou88C39 <kanfutrooper@icloud.com>
2023-12-23 14:48:28 +0900

    Edit make-todo/make-todo.resolvers.ts

--- packages/backend/src/modules/todos/get-todos/get-todos.resolvers.ts
+++ packages/backend/src/modules/todos/get-todos/get-todos.resolvers.ts
@@ -2,12 +2,12 @@ import { Resolvers } from '../../../_generated_/graphql.js';
 import { MyContext } from '../../../types/graphql.js';
 
 export const resolvers: Resolvers<MyContext> = {
-    Query:{
-        getTodos: async
-        (_, args, {PrismaClient}, info) => {
-        return {
-            todos:[],
-        };
+  Query: {
+    getTodos: async (_, args, { PrismaClient }, info) => {
+      const todos = await context.PrismaClient.todo.findMany();
+      return {
+        todos: todos,
+      };
     },
   },
 };
