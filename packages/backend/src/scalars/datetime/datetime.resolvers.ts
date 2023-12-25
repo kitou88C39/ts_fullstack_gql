336b2848e0f902230c4283a2ff5b7d06a0d8232b
kitou88C39 <kanfutrooper@icloud.com>
2023-12-25 09:34:20 +0900

    Add scalars/datetime folder and datetime.resolvers.ts in root folder

--- packages/backend/src/scalars/datetime/datetime.resolvers.ts
+++ packages/backend/src/scalars/datetime/datetime.resolvers.ts
@@ -1,6 +1,18 @@
 import { GraphQLScalarType } from 'graphql';
 import { Resolvers } from '../../_generated_/graphql.js';
 
+function validateDateTime(value: string) {
+  const RFC_3339_REGEX =
+    /^(\d{4}-(0[1-9]|1[012])-(0[1-9]|[12][0-9]|3[01])T([01][0-9]|2[0-3]):([0-5][0-9]):([0-5][0-9]|60))(\.\d{1,})?(([Z])|([+|-]([01][0-9]|2[0-3]):[0-5][0-9]))$/;
+
+  const dateTimeString = value.toUpperCase();
+
+  if (!RFC_3339_REGEX.test(dateTimeString)) {
+    return false;
+  }
+  return true;
+}
+
 export const resolvers: Resolvers = {
   DateTime: new GraphQLScalarType({
     name: 'DateTime',
