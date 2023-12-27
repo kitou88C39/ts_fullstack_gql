c525f31923bed28a5190aff396887bfadb36c525
kitou88C39 <kanfutrooper@icloud.com>
2023-12-27 16:35:19 +0900

    frontend/src/main.tsx

--- packages/frontend/src/main.tsx
+++ packages/frontend/src/main.tsx
@@ -1,10 +1,19 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+declare module 'react-dom/client';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.tsx';
+import './index.css';
+import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
+
+const apolloClient = new ApolloClient({
+  uri: 'http://localhost:5555/graphql', // Adjust the GraphQL endpoint URL as needed
+  cache: new InMemoryCache(),
+});
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <ApolloProvider client={apolloClient}>
+      <App />
+    </ApolloProvider>
+  </React.StrictMode>
+);
