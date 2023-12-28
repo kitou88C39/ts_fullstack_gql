b6297083997c4220e9967fdaff91d63489d24a99
kitou88C39 <kanfutrooper@icloud.com>
2023-12-28 09:20:57 +0900

    Edit frontend/src/App.tsx

--- packages/frontend/src/App.tsx
+++ packages/frontend/src/App.tsx
@@ -15,7 +15,17 @@ const getTodoQuery = gql`
 `;
 
 function App() {
-  return <div className='App'></div>;
+  const { data, loading, error } = useQuery(getTodoQuery);
+
+  return (
+    <div className='App'>
+      <div>
+        {data?.getTodos?.todos.map((item: any) => (
+          <p key={item.id}>{item.title}</p>
+        ))}
+      </div>
+    </div>
+  );
 }
 
 export default App;
